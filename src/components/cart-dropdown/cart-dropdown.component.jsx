6b6529d6a5a2e78e06fb5f753b9f9702006bc346
Trung <tqtrung1209@gmail.com>
2021-03-04 11:16:43 +0700

    add cart icon and cart dropdown to app, cart reducer

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import './cart-dropdown.styles.scss';
+
+const CartDropDown = () => (
+    <div className='cart-dropdown'>
+        <div className='cart-item' />
+        <CustomButton>GO CHECK OUT</CustomButton>
+    </div>
+)
+export default CartDropDown;
