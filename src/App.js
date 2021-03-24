904f38d963eb2e4e72cb4f316e7229377b7aa12e
Trung <tqtrung1209@gmail.com>
2021-03-24 13:43:55 +0700

    remove shopdata on app, added withspinner for loading phase

--- src/App.js
+++ src/App.js
@@ -12,10 +12,12 @@ import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selector';
+
 class App extends React.Component {
 
   unsubscribeFromAuth = null
   componentDidMount() {
+
     const {setCurrentUser} = this.props;
     this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth => {
       if(userAuth) {
@@ -31,6 +33,7 @@ class App extends React.Component {
 
       }
       setCurrentUser(userAuth);
+
     }
       
       );
@@ -57,10 +60,11 @@ class App extends React.Component {
 }
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
+
 })
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
-
+  
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
