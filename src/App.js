bbbc17af904cceaa13c5907a70f6e721c008b46e
Trung <tqtrung1209@gmail.com>
2021-07-17 17:06:45 +0700

    add IV

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import CheckoutPage from './pages/checkout/checkout.component'
 import { createStructuredSelector } from 'reselect';
-import './App.css';
 import Header from './components/header/header.component';
+
+import { GlobalStyle } from './global.styles';
+
 import {checkUserSession} from './redux/user/user.action';
 
 import { connect } from 'react-redux';
@@ -23,6 +25,7 @@ const App = ({checkUserSession,currentUser})=> {
 
   return (
     <div>
+      <GlobalStyle/>
       <Header />
       <Switch>
       <Route exact path='/' component={Homepage}/>  
